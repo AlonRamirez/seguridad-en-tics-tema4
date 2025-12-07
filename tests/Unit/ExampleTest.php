1aadd7f4b00cd5d2968411d1f9fa671b318c4f96
AlonRamirez <alonsormz.mail@gmail.com>
2025-12-07 14:35:52 -0600

    cambios realizados

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,5 +1,23 @@
 <?php
 
-test('that true is true', function () {
-    expect(true)->toBeTrue();
-});
+namespace Tests\Unit;
+
+use App\Http\Controllers\OperationsController;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_int_addition_result(): void
+    {
+        $controller = new OperationsController;
+
+        $this->assertIsInt($controller->addition(5, 6));
+        $this->assertNotNull($controller->addition(5, 6));
+        $this->assertGreaterThan(5, $controller->addition(5, 6));
+    }
+}
