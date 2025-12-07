e650e8d3256f31ec1a0b8dd39114b7d978724ac5
AlonRamirez <alonsormz.mail@gmail.com>
2025-12-06 23:45:57 -0600

    agregar cambios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot():void
     {
-        //
+        if (app()->environment('testing')) {
+            config()->set('vite.manifest', false);
+        }
     }
 }
---
+++ config/testing.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'disable_vite' => true,
+];
+
+?>
